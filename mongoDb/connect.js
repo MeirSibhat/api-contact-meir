3a36d62191c26b59b6a2a73e26ea91f1f0cc567f
MeirSibhat <meirsibhat@gmail.com>
2020-08-29 17:01:21 +0300

    first commit

--- mongoDb/connect.js
+++ mongoDb/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const config = require("config");
+//There is no link to the database for security reasons =>  config
 let urlConnect = `${config.get("mongoServer")}` // קריאה נתונים מהשרת של הדאטה 
 mongoose.connect(urlConnect, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
